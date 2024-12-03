027c003bddf7ed199c8309261dbac1e736800160
Michael Kashirin <m.k4shirin@yandex.ru>
2024-12-04 00:13:11 +0300

    Fix test_sbf.sh script

--- test_sbf.sh
+++ test_sbf.sh
@@ -6,7 +6,7 @@ cargo build-sbf
 
 # Dump the Metaplex Metadata program to the fixtures directory, so that it can
 # be added to the test environment
-if [ ! -f "/target/deploy/metaplex_token_metadata_program.so" ]; then
+if [ ! -f "./target/deploy/metaplex_token_metadata_program.so" ]; then
     solana program dump \
         --url="mainnet-beta" \
         "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s" \
